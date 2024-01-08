6edc69869b5efae2be7173fe4d5ee033d6143f93
Sash <savic.s82@gmail.com>
2024-01-08 12:47:16 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-print("Hello from Lake")
+# Copyright 2024 Telstra.com, Pty. Ltd. or its affiliates. All Rights Reserved.
+
+import aws_cdk as cdk
+from constructs import Construct
+
+class EmptyStack(cdk.Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        pass
+
+
+print("Hello from StarkLake... Bootstrapping an EmptyStack")
+app = cdk.App()
+
+EmptyStack(app, 'StackStub')
+
+app.synth()
+
+
+
